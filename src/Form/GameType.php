c02adecbf7ec049b1361b17c2ae7184b23650272
Denys Stashchenko <derdek02@gmail.com>
2024-09-26 15:33:15 +0300

    create game crud

---
+++ src/Form/GameType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use App\Entity\Player;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('password')
+            ->add('max_players')
+            ->add('created_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('ended_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('player', EntityType::class, [
+                'class' => Player::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Game::class,
+        ]);
+    }
+}
